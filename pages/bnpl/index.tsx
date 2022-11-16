e4eae1d1ff7ced8e208b19c316569ce15fe11d1e
Anh Ly <anh.ly@credify.one>
2022-11-16 11:39:11 +0700

    update pj

--- pages/bnpl/index.tsx
+++ pages/bnpl/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../../styles/Home.module.css'
-import { RegistrationForm } from '../components/form'
+import { RegistrationForm } from '../../components/form'
 
 const Bnpl: NextPage = () => {
   const onSubmit = (values: any) => {
